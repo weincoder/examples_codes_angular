a296b2eb5e2fced2108c2e8b5fa5ebb4fb05b37f
daniherr <daniherr@bancolombia.com.co>
2022-09-20 12:20:48 -0500

    subiendo repo

--- micro-fronts/core/src/test/models/album.test.ts
+++
@@ -1,6 +0,0 @@
-
-import {describe, expect, test} from '@jest/globals';
-import {Album} from '../models/album';
-test('Test Album', () => {
-    Album()
-});
---
+++ micro-fronts/core/test/models/album.test.ts
@@ -0,0 +1,14 @@
+
+import {describe, expect, test} from '@jest/globals';
+import {Album} from '../../src/models/album';
+test('Test Album the hardest test xD 🤣 ', () => {
+    //Arrenge
+    let fakeAlbum = new Album();
+    let expectedTitle = 'a little fake Album😂';
+    //Act
+    fakeAlbum.id = 2;
+    fakeAlbum.title = expectedTitle;
+    fakeAlbum.userId = 14;
+    //Assert
+    expect(expectedTitle).toEqual(fakeAlbum.title);
+});
